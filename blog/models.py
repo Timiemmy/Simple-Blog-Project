e1f7886dd7c4482193cd21b2654dee91a3ef4802
Timiemmy <faith.bolanle@yahoo.com>
2023-03-01 11:32:12 +0100

    Add views, urls and templates to it with friendly SEO url

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
+# Adding Publish manager which will allow us to retrieve all post using the notion Post.published.all() in views
 
-class PublishManager(models.Manager):
+
+class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
@@ -12,12 +15,12 @@ class Post(models.Model):
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
-        PUBLISH = 'PB', 'Publish'
+        PUBLISHED = 'PB', 'Published'
 
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='blog_posts')
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     content = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -26,13 +29,17 @@ class Post(models.Model):
         max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     objects = models.Manager()
-    published = PublishManager
+    published = PublishedManager()
 
     class Meta:
+        # This will order the posts by the publish date
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish'])
-        ]
+        ]  # This will order the post in database
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):  # Using canonical url
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/list.html", {"posts": posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__year=year, publish__month=month, publish__day=day)
+    return render(request, "blog/detail.html", {"post": post})
